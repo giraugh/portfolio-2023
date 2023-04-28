23615a163faadf695d5d18f092f9cd616347a142
Ewan Breakey <ebreakey@gmail.com>
2023-04-28 18:40:52 +1000

    Create gallery data and layout

--- src/routes/gallery/+page.svelte
+++ src/routes/gallery/+page.svelte
@@ -1,1 +1,140 @@
-gallery here!
+<script lang="ts">
+	import galleryAreas, { type GalleryImage } from '$lib/data/gallery'
+	import { makeImageLink } from '$lib/data/projects'
+
+	let focusedImage: null | GalleryImage = null
+	let dialog: HTMLDialogElement
+
+	const focusImage = (image: GalleryImage) => {
+		focusedImage = image
+		dialog.showModal()
+	}
+
+	const unfocusImage = () => {
+		focusedImage = null
+		dialog.close()
+	}
+</script>
+
+<div class="gallery">
+	<h1>Gallery</h1>
+	<p>
+		A showcase of my design and illustration work for various university communities, discord
+		servers and personal projects. Primarily features vector and pixel art pieces.
+	</p>
+	{#each galleryAreas as area}
+		<section>
+			<h3>{area.name}</h3>
+			<p>{area.description}</p>
+			<ul class:grid={area.showAsGrid}>
+				{#each area.images as image}
+					<li class:square={image.isSquare}>
+						<button on:click={() => focusImage(image)}>
+							<img src={makeImageLink(image.imageId)} alt={image.name} title={image.name} />
+						</button>
+					</li>
+				{/each}
+			</ul>
+		</section>
+	{/each}
+
+	<!-- Focused Image Modal -->
+	<dialog
+		bind:this={dialog}
+		on:click={() => unfocusImage()}
+		on:keydown={(e) => e.key === 'Escape' && unfocusImage()}
+	>
+		{#if focusedImage}
+			<img src={makeImageLink(focusedImage.imageId)} alt={focusedImage.name} />
+			<strong>{focusedImage.name}</strong>
+		{/if}
+	</dialog>
+</div>
+
+<style lang="scss">
+	.gallery {
+		padding: 2em;
+	}
+
+	dialog {
+		position: fixed;
+		inset: 3em;
+		border: none;
+		background: none;
+		cursor: zoom-out;
+
+		&::backdrop {
+			background: rgba(0, 0, 0, 0.6);
+			cursor: zoom-out;
+		}
+
+		img {
+			display: block;
+			width: 100%;
+			max-height: 80vh;
+		}
+
+		strong {
+			color: white;
+			font-size: 2rem;
+		}
+	}
+
+	section {
+		margin-block: 4em;
+	}
+
+	ul {
+		display: flex;
+		flex-wrap: wrap;
+		list-style-type: none;
+		padding: 0;
+		gap: 1em;
+
+		&.grid {
+			display: grid;
+			grid-template-columns: repeat(auto-fit, minmax(10em, 1fr));
+
+			li {
+				max-height: initial;
+				width: 100%;
+			}
+		}
+	}
+
+	li {
+		max-height: 10em;
+
+		&:not(.square) {
+			flex: 1;
+			min-width: 15em;
+		}
+
+		&.square {
+			aspect-ratio: 1;
+		}
+
+		button {
+			width: 100%;
+			height: 100%;
+			margin: 0;
+			padding: 0;
+			border: 0;
+			overflow: hidden;
+		}
+	}
+
+	li img {
+		display: block;
+		width: 100%;
+		height: 100%;
+		object-fit: cover;
+		border-radius: 0.2em;
+
+		transition: transform 0.2s;
+		&:hover {
+			cursor: zoom-in;
+			transform: scale(1.075);
+		}
+	}
+</style>
